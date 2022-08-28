e0258b4dd42e3db0b15b37cf0111fee1eb530368
almayor <mayorovme@gmail.com>
2022-08-28 17:27:11 +0300

    р

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,38 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy as B
 import System.Environment
 
-import Lib
 import Types
 import Engine
 
+import Prettyprinter
 import Control.Monad.Except
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr, hPrint)
+import Control.Monad.Except
+
+printUsage :: IO ()
+printUsage = do
+    progName <- getProgName
+    fail $
+        "Usage: " ++ progName ++ " [-h] jsonfile input\
+        \\n\
+        \positional arguments:\n\
+        \jsonfile              json description of the machine\n\
+        \\n\
+        \input                 input of the machine\n\
+        \\n\
+        \optional arguments:\n\
+        \-h, --help            show this help message and exit"
 
 main :: IO ()
 main = run `catchError` handler where
-    handler e = putStrLn $ "Error: " ++ show e
+    handler e = hPrint stderr e >> exitFailure
     run = do
         args <- getArgs
+        let helpRequested = "--help" `elem` args || "-h" `elem` args
+        when (length args /= 2 || helpRequested) printUsage
         contents <- B.readFile (head args)
         let specif = eitherDecode contents :: Either String Specification
-        print specif
-        -- maybe (putStrLn "couldn't parse") print specif 
+        either fail (print . pretty) specif
 
 
--- src/Types.hs
+++ src/Types.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Types where
 
 import Prelude hiding (read)
 import GHC.Generics
 import Data.Aeson
-import Data.Map (Map)
+import Data.Map (Map, toList)
+import Data.Semigroup (stimes)
+
+import Prettyprinter
 
 type StateName = String
 type Symbol = String
@@ -30,3 +34,40 @@ data Specification = Specification {
     finals      :: [StateName],
     transitions :: Map StateName [Transition]
 }  deriving (Eq, Show, Generic, ToJSON, FromJSON)
+
+instance Pretty Action where
+    pretty = pretty . show
+
+instance Pretty Specification where
+    pretty spec = vsep
+        [ printName
+        , pretty "Alphabet:" <+> prettyList' (alphabet spec)
+        , pretty "States:" <+> prettyList' (states spec)
+        , pretty "Initial:" <+> pretty (initial spec)
+        , pretty "Finals:" <+> prettyList' (finals spec)
+        , pretty "Transitions:"
+        , indent 2 . vsep . map printTransitions . toList $ transitions spec ]
+        where
+        prettyList' = encloseSep' "[ " " ]" ", " . map pretty
+        encloseSep' a b c = encloseSep (pretty a) (pretty b) (pretty c)
+        printTransition from_state (Transition {read, write, to_state, action}) =
+                pretty (from_state, read)
+            <+> pretty "->"
+            <+> pretty (to_state, write, action)
+        printTransitions (state, transitions) =
+            vsep $ map (printTransition state) transitions
+        printName =
+            let ast = pretty '*'
+                w = 80 :: Int
+                nameWidth = length (name spec)
+                pad = (w - nameWidth) `div` 2
+            in vsep
+            [ stimes w ast
+            , ast <> fill (w - 2) (stimes pad space <> pretty (name spec)) <> ast
+            , stimes w ast ]
+            
+
+        
+
+       
+
